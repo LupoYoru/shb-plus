c5cd795339856d851cd415c6c02ba083837f04ea
LupoYoru <74294711+LupoYoru@users.noreply.github.com>
2024-09-07 20:31:13 +0200

    v0.8.6-5
    Downtime-Aktivitäten Ergänzungen zu Komplikationen und Grubenkämpfen; Verschiedene Fehlerbehebungen & Korrekturen

--- data/makro_klassenwahl.js
+++ data/makro_klassenwahl.js
@@ -6,49 +6,49 @@ await Dialog.prompt({
         callback: async(html) => {
           let select = html.find('[name="klassenwahl"]').val();
           if (select === "barbar") {
-            let msg_barbar = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Barbar</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 2d4 * 2 #Anfangsvermögen als Barbar]]</p>`;
+            let msg_barbar = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Barbar</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 2d4 * 10 #Anfangsvermögen als Barbar]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_barbar
             })
           }
           if (select === "barde") {
-            let msg_barde = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Barde</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 2 #Anfangsvermögen als Barde]]</p>`;
+            let msg_barde = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Barde</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 10 #Anfangsvermögen als Barde]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_barde
             })
           }
           if (select === "druide") {
-            let msg_druide = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Druide</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 2d4 * 2 #Anfangsvermögen als Druide]]</p>`;
+            let msg_druide = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Druide</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 2d4 * 10 #Anfangsvermögen als Druide]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_druide
             })
           }
           if (select === "hexenmeister") {
-            let msg_hexenmeister = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Hexenmeister</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 4d4 * 2 #Anfangsvermögen als Druide]]</p>`;
+            let msg_hexenmeister = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Hexenmeister</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 4d4 * 10 #Anfangsvermögen als Hexenmeister]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_hexenmeister
             })
           }
           if (select === "kaempfer") {
-            let msg_kaempfer = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Kämpfer</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 2 #Anfangsvermögen als Kämpfer]]</p>`;
+            let msg_kaempfer = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Kämpfer</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 10 #Anfangsvermögen als Kämpfer]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_kaempfer
             })
           }
           if (select === "kleriker") {
-            let msg_kleriker = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Kleriker</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 2 #Anfangsvermögen als Kleriker]]</p>`;
+            let msg_kleriker = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Kleriker</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 10 #Anfangsvermögen als Kleriker]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_kleriker
             })
           }
           if (select === "magier") {
-            let msg_magier = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Magier</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 4d4 * 2 #Anfangsvermögen als Magier]]</p>`;
+            let msg_magier = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Magier</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 4d4 * 10 #Anfangsvermögen als Magier]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_magier
@@ -62,28 +62,28 @@ await Dialog.prompt({
             })
           }
           if (select === "paladin") {
-            let msg_paladin = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Paladin</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 2 #Anfangsvermögen als Paladin]]</p>`;
+            let msg_paladin = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Paladin</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 10 #Anfangsvermögen als Paladin]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_paladin
             })
           }
           if (select === "schurke") {
-            let msg_schurke = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Schurke</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 4d4 * 2 #Anfangsvermögen als Schurke]]</p>`;
+            let msg_schurke = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Schurke</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 4d4 * 10 #Anfangsvermögen als Schurke]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_schurke
             })
           }
           if (select === "waldlaeufer") {
-            let msg_waldlaeufer = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Waldläufer</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 2 #Anfangsvermögen als Waldläufer]]</p>`;
+            let msg_waldlaeufer = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Waldläufer</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 5d4 * 10 #Anfangsvermögen als Waldläufer]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_waldlaeufer
             })
           }
           if (select === "zauberer") {
-            let msg_zauberer = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Zauberer</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 3d4 * 2 #Anfangsvermögen als Zauberer]]</p>`;
+            let msg_zauberer = `<p style="text-align: center;">Ermittle dein <strong>Anfangsvermögen als Zauberer</strong> durch folgenden Würfelwurf:</p><p style="text-align: center;">[[/r 3d4 * 10 #Anfangsvermögen als Zauberer]]</p>`;
             ChatMessage.create({
                 user: game.user._id,
                 content: msg_zauberer
